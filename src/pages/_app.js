40cf32d49c59dff4196c68e4da8c36ea6a45cf45
calebbenjin <calebben94@gmail.com>
2021-11-23 16:14:36 +0100

    react-modal-css commented

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import 'react-multi-carousel/lib/styles.css';
-import 'react-modal-video/css/modal-video.min.css';
+// import 'react-modal-video/css/modal-video.min.css';
 import 'rc-drawer/assets/index.css';
 import 'typeface-dm-sans';
 
